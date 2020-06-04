7c384f1fc196fb1de291d433d8229a6d753d77a6
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-04 16:03:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,21 +1,18 @@
 # Your code goes here!
 require 'pry'
 
-
 class Anagram
   attr_accessor :word
-
+  
   def initialize(word)
     @word = word
   end
 
   def match(array)
-    word_array = @word.split("")
-    array.select do |w|
-      w_array = w.split("")
-      w_array.sort == word_array.sort
-      
+    array.select do |word|
+      word.sort == @word.sort
     end
   end
 
+  
 end
